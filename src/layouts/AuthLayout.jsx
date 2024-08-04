bf6a47f0716059530842ec9f461315af24d7eb6b
milagrosb02 <milagrosbordon9@gmail.com>
2024-08-04 17:32:42 -0300

    logotipo agregado

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,20 @@ import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
     return (
-        <div>
-            AuthLayout
-            <Outlet />
-        </div>
+        <main className='max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center'>
+            {/* Logo */}
+            <img
+                src='../img/logo.svg'
+                alt='imagen logotipo'
+                className='max-w-xs'
+            />
+
+            <div className='p-10 w-full'>
+             <Outlet />
+            </div>
+
+            
+        </main>
     );
 }
 
