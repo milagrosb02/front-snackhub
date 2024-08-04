0058c2453d579ed3f5b0328071b6d426b380e455
milagrosb02 <milagrosbordon9@gmail.com>
2024-08-04 18:25:49 -0300

    formulario de login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,57 @@
 
 const Login = () => {
     return (
-        <div>
-            Hola Login
-        </div>
+         // etiqueta fragment (retornar etiquetas sin html)
+       <>
+       <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+       <p>Para crear un pedido, inicia sesión</p>
+
+       <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+
+           <form>
+
+               
+               {/* email */}
+               <div className="mb-4">
+                  <label className="text-slate-800" htmlFor="email">
+                   Email:
+                   </label> 
+
+                   <input type="email"
+                       id="email"
+                       className="mt-2 w-full p-3 bg-gray-50"
+                       name="email"
+                       placeholder="Tu Email"
+                   />
+
+               </div>
+
+               {/* password */}
+               <div className="mb-4">
+                  <label className="text-slate-800" htmlFor="password">
+                   Password:
+                   </label> 
+
+                   <input type="password"
+                       id="password"
+                       className="mt-2 w-full p-3 bg-gray-50"
+                       name="password"
+                       placeholder="Tu Password"
+                   />
+
+               </div>
+
+
+               <input 
+                   type="submit"
+                   value="Iniciar Sesión"
+                   className="bg-orange-400 hover:bg-orange-600 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+               />
+
+           </form>
+           
+       </div>
+  </>
     );
 }
 
